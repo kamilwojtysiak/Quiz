3942f46ff3d2ddbd619afc06ad19fb475ddb53d8
kamilwojtysiak <kamil@futrzane.pl>
2020-06-11 08:56:50 +0200

    Conversion of DataTable into List

--- QUIZ/Question.cs
+++ QUIZ/Question.cs
@@ -6,9 +6,8 @@ namespace QUIZ
 {
     public class Question
     {
-        public Answer[] Answers;
+        public List<Answer> answers;
         public string Correctanswer;
-
         public string Text { get; set; }
         public override string ToString()
         {
--- QUIZ/Tools.cs
+++ QUIZ/Tools.cs
@@ -14,139 +14,48 @@ namespace QUIZ
         }
         public static void AskingQuestions()
         {
-            var questions = new Question[]
-            {
-                new Question
-                {
-                    Correctanswer = "D",
-                    Text = "1. Jak długo Amazon działa na polskim rynku?",
-                    Answers = new Answer[]
-                    {
-                        new Answer
-                        {
-                            Text = "A) rok",
-                        },
-                        new Answer
-                        {
-                            Text = "B) 2 lata",
-                        },
-                        new Answer
-                        {
-                            Text = "C) 5 lat",
-                        },
-                        new Answer
-                        {
-                            Text = "D) Amazon nie działa w Polsce",
-                        }
-                    }
-                },
-                new Question
-                {
-                    Correctanswer = "B",
-                    Text = "2. Kto jest założycielem Amazon.com?",
-                    Answers = new Answer[]
-                    {
-                        new Answer
-                        {
-                            Text = "A) Steve Jobs",
-                        },
-                        new Answer
-                        {
-                            Text = "B) Jeff Bezos",
-                        },
-                        new Answer
-                        {
-                            Text = "C) Al Pacino",
-                        },
-                        new Answer
-                        {
-                            Text = "D) Diego Simeone",
-                        }
-                    }
-                },
-                new Question
-                {
-                    Correctanswer = "B",
-                    Text = "3. Jak nazywa się jeden z głównych modeli sprzedaży na Amazon.com",
-                    Answers = new Answer[]
-                    {
-                        new Answer
-                        {
-                            Text = "A) KFC",
-                        },
-                        new Answer
-                        {
-                            Text = "B) FBA",
-                        },
-                        new Answer
-                        {
-                            Text = "C) ZTM",
-                        },
-                        new Answer
-                        {
-                            Text = "D) RPK",
-                        }
-                    }
-                },
-                new Question
-                {
-                    Correctanswer = "B",
-                    Text = "4. Czego sprzedażą początkowo zajmował się Amazon?",
-                    Answers = new Answer[]
-                    {
-                        new Answer
-                        {
-                            Text = "A) Artykuły biurowe",
-                        },
-                        new Answer
-                        {
-                            Text = "B) Książki",
-                        },
-                        new Answer
-                        {
-                            Text = "C) Narzędzia",
-                        },
-                        new Answer
-                        {
-                            Text = "D) Płyty CD",
-                        }
-                    }
-                },
-                new Question
-                {
-                    Correctanswer = "D",
-                    Text = "5. Jaka firma była głównym konkurentem Amazona w Stanach Zjednoczonych?",
-                    Answers = new Answer[]
-                    {
-                        new Answer
-                        {
-                            Text = "A) OLX",
-                        },
-                        new Answer
-                        {
-                            Text = "B) Allegro",
-                        },
-                        new Answer
-                        {
-                            Text = "C) Etsy",
-                        },
-                        new Answer
-                        {
-                            Text = "D) e-Bay",
-                        }
-                    }
-                }
-            };
+            List<Question> questions = new List<Question>();
+
+            questions.Add(new Question { Text = "1. Jak długo Amazon działa na polskim rynku?", Correctanswer = "D", answers = new List<Answer>() });
+            questions.Add(new Question { Text = "2. Kto jest założycielem Amazon.com?", Correctanswer = "B", answers = new List<Answer>() });
+            questions.Add(new Question { Text = "3. Jak nazywa się jeden z głównych modeli sprzedaży na Amazon.com", Correctanswer = "B", answers = new List<Answer>() });
+            questions.Add(new Question { Text = "4. Czego sprzedażą początkowo zajmował się Amazon?", Correctanswer = "B", answers = new List<Answer>() });
+            questions.Add(new Question { Text = "5. Jaka firma była głównym konkurentem Amazona w Stanach Zjednoczonych?", Correctanswer = "D", answers = new List<Answer>() });
+
+            questions[0].answers.Add(new Answer { Text = "A) rok" });
+            questions[0].answers.Add(new Answer { Text = "B) 2 lata" });
+            questions[0].answers.Add(new Answer { Text = "C) 5 lat" });
+            questions[0].answers.Add(new Answer { Text = "D) Amazon nie działa w Polsce" });
+
+            questions[1].answers.Add(new Answer { Text = "A) Steve Jobs" });
+            questions[1].answers.Add(new Answer { Text = "B) Jeff Bezos" });
+            questions[1].answers.Add(new Answer { Text = "C) Al Pacino" });
+            questions[1].answers.Add(new Answer { Text = "D) Diego Simeone" });
+
+            questions[2].answers.Add(new Answer { Text = "A) KFC" });
+            questions[2].answers.Add(new Answer { Text = "B) FBA" });
+            questions[2].answers.Add(new Answer { Text = "C) RPA" });
+            questions[2].answers.Add(new Answer { Text = "D) ZTM" });
+
+            questions[3].answers.Add(new Answer { Text = "A) Artykuły buirowe" });
+            questions[3].answers.Add(new Answer { Text = "B) Książki" });
+            questions[3].answers.Add(new Answer { Text = "C) Płyty CD" });
+            questions[3].answers.Add(new Answer { Text = "D) Narzędzia" });
+
+            questions[4].answers.Add(new Answer { Text = "A) Allegro" });
+            questions[4].answers.Add(new Answer { Text = "B) Etsy" });
+            questions[4].answers.Add(new Answer { Text = "C) OLX" });
+            questions[4].answers.Add(new Answer { Text = "D) e-Bay" });
 
             int result = 0;
-
+            
             foreach (var question in questions)
             {
-                Console.WriteLine(question);
+                Console.WriteLine(question.Text);
 
-                foreach (var answer in question.Answers)
+                foreach (var answer in question.answers)
                 {
-                    Console.WriteLine(answer);
+                    Console.WriteLine(answer.Text);
                 }
 
                 Console.Write("Answer: ");
@@ -164,11 +73,8 @@ namespace QUIZ
                 }
             }
 
-            Console.WriteLine();
-
-            Tools.ColorWriteLine($"Your result is: {result} points", ConsoleColor.Blue);
+            Tools.ColorWriteLine($"You got {result}, its {Math.Round((decimal)result / questions.Count * 100)}%", ConsoleColor.Blue);
         }
     }
-
-
 }
+
