04f47236b35add57adbcb2428d58c12c536cef88
kamilwojtysiak <kamil@futrzane.pl>
2020-06-11 09:28:11 +0200

    Little change

--- QUIZ/Tools.cs
+++ QUIZ/Tools.cs
@@ -15,42 +15,42 @@ namespace QUIZ
         
         public static void AskingQuestions()
         {
-            List<Question> question = new List<Question>();
+            List<Question> questions = new List<Question>();
 
-            question.Add(new Question { Text = "1. Jak długo Amazon działa na polskim rynku?", Correctanswer = "D", answers = new List<Answer>() });
-            question.Add(new Question { Text = "2. Kto jest założycielem Amazon.com?", Correctanswer = "B", answers = new List<Answer>() });
-            question.Add(new Question { Text = "3. Jak nazywa się jeden z głównych modeli sprzedaży na Amazon.com", Correctanswer = "B", answers = new List<Answer>() });
-            question.Add(new Question { Text = "4. Czego sprzedażą początkowo zajmował się Amazon?", Correctanswer = "B", answers = new List<Answer>() });
-            question.Add(new Question { Text = "5. Jaka firma była głównym konkurentem Amazona w Stanach Zjednoczonych?", Correctanswer = "D", answers = new List<Answer>() });
+            questions.Add(new Question { Text = "1. Jak długo Amazon działa na polskim rynku?", Correctanswer = "D", answers = new List<Answer>() });
+            questions.Add(new Question { Text = "2. Kto jest założycielem Amazon.com?", Correctanswer = "B", answers = new List<Answer>() });
+            questions.Add(new Question { Text = "3. Jak nazywa się jeden z głównych modeli sprzedaży na Amazon.com", Correctanswer = "B", answers = new List<Answer>() });
+            questions.Add(new Question { Text = "4. Czego sprzedażą początkowo zajmował się Amazon?", Correctanswer = "B", answers = new List<Answer>() });
+            questions.Add(new Question { Text = "5. Jaka firma była głównym konkurentem Amazona w Stanach Zjednoczonych?", Correctanswer = "D", answers = new List<Answer>() });
 
-            question[0].answers.Add(new Answer { Text = "A) rok" });
-            question[0].answers.Add(new Answer { Text = "B) 2 lata" });
-            question[0].answers.Add(new Answer { Text = "C) 5 lat" });
-            question[0].answers.Add(new Answer { Text = "D) Amazon nie działa w Polsce" });
+            questions[0].answers.Add(new Answer { Text = "A) rok" });
+            questions[0].answers.Add(new Answer { Text = "B) 2 lata" });
+            questions[0].answers.Add(new Answer { Text = "C) 5 lat" });
+            questions[0].answers.Add(new Answer { Text = "D) Amazon nie działa w Polsce" });
 
-            question[1].answers.Add(new Answer { Text = "A) Steve Jobs" });
-            question[1].answers.Add(new Answer { Text = "B) Jeff Bezos" });
-            question[1].answers.Add(new Answer { Text = "C) Al Pacino" });
-            question[1].answers.Add(new Answer { Text = "D) Diego Simeone" });
+            questions[1].answers.Add(new Answer { Text = "A) Steve Jobs" });
+            questions[1].answers.Add(new Answer { Text = "B) Jeff Bezos" });
+            questions[1].answers.Add(new Answer { Text = "C) Al Pacino" });
+            questions[1].answers.Add(new Answer { Text = "D) Diego Simeone" });
 
-            question[2].answers.Add(new Answer { Text = "A) KFC" });
-            question[2].answers.Add(new Answer { Text = "B) FBA" });
-            question[2].answers.Add(new Answer { Text = "C) RPA" });
-            question[2].answers.Add(new Answer { Text = "D) ZTM" });
+            questions[2].answers.Add(new Answer { Text = "A) KFC" });
+            questions[2].answers.Add(new Answer { Text = "B) FBA" });
+            questions[2].answers.Add(new Answer { Text = "C) RPA" });
+            questions[2].answers.Add(new Answer { Text = "D) ZTM" });
 
-            question[3].answers.Add(new Answer { Text = "A) Artykuły buirowe" });
-            question[3].answers.Add(new Answer { Text = "B) Książki" });
-            question[3].answers.Add(new Answer { Text = "C) Płyty CD" });
-            question[3].answers.Add(new Answer { Text = "D) Narzędzia" });
+            questions[3].answers.Add(new Answer { Text = "A) Artykuły buirowe" });
+            questions[3].answers.Add(new Answer { Text = "B) Książki" });
+            questions[3].answers.Add(new Answer { Text = "C) Płyty CD" });
+            questions[3].answers.Add(new Answer { Text = "D) Narzędzia" });
 
-            question[4].answers.Add(new Answer { Text = "A) Allegro" });
-            question[4].answers.Add(new Answer { Text = "B) Etsy" });
-            question[4].answers.Add(new Answer { Text = "C) OLX" });
-            question[4].answers.Add(new Answer { Text = "D) e-Bay" });
+            questions[4].answers.Add(new Answer { Text = "A) Allegro" });
+            questions[4].answers.Add(new Answer { Text = "B) Etsy" });
+            questions[4].answers.Add(new Answer { Text = "C) OLX" });
+            questions[4].answers.Add(new Answer { Text = "D) e-Bay" });
 
             int result = 0;
             
-            foreach (var question in question)
+            foreach (var question in questions)
             {
                 Console.WriteLine(question.Text);
 
@@ -74,7 +74,7 @@ namespace QUIZ
                 }
             }
 
-            Tools.ColorWriteLine($"You got {result} point, its {Math.Round((decimal)result / question.Count * 100)}%", ConsoleColor.Blue);
+            Tools.ColorWriteLine($"You got {result} point, its {Math.Round((decimal)result / questions.Count * 100)}%", ConsoleColor.Blue);
         }
     }
 }
